3b126a403225547f089713666989081058579bdb
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-04-06 22:47:55 +0900

    feat: add calculate passage day.🐥

--- src/page/top/Top.tsx
+++ src/page/top/Top.tsx
@@ -1,12 +1,14 @@
 import React, { Dispatch, useReducer } from 'react';
 import { InputForm } from './components/InputForm';
 import { initialState, formReducer, FormState } from '../../reducers/FormReducer';
+import { differenceInDays, parseISO } from 'date-fns';
 
 export const Top = () => {
   const FormContext = React.createContext<FormState>(null as any);
   const DispatchContext = React.createContext<Dispatch<any>>(null as any);
   const [state, dispatch] = useReducer(formReducer, initialState);
-
+  // TODO: 日付処理はどこかに移動
+  const calculatePassage = (date: string) => `${differenceInDays(new Date(), parseISO(date))}日`;
   return (
     <FormContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
@@ -18,6 +20,8 @@ export const Top = () => {
           <dd>{item.value}</dd>
           <dt>date:</dt>
           <dd>{item.date}</dd>
+          <dt>経過日数</dt>
+          <dd>{calculatePassage(item.date)}</dd>
         </dl>
       ))}
     </FormContext.Provider>
