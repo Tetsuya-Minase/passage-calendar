ecdd3b30d65e9746ec43bcbf56b3a92912317a6d
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-06-11 21:37:44 +0900

    feat: fix config typo.🔧

--- src/common/util/Firebase.ts
+++ src/common/util/Firebase.ts
@@ -6,8 +6,11 @@ import 'firebase/database';
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  databaseUrl: process.env.DATABASE_URL
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(config);
