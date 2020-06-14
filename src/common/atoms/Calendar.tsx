7b5b89b9fa31b83e3cb1f53ba54ca36d8f21f562
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-06-14 21:47:06 +0900

    feat: fix calendar styles.🎨

--- src/common/atoms/Calendar.tsx
+++ src/common/atoms/Calendar.tsx
@@ -1,6 +1,15 @@
 import React, { useCallback } from 'react';
 import Calendar from 'react-calendar';
+import styled from 'styled-components';
 import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
+
+const CalendarWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
 
 type IProps = {
   [key: string]: Document;
@@ -13,6 +22,6 @@ export const CalendarComponent: React.FC<IProps> = (props) => {
     const dateKey = date.toString();
     return tileContent[dateKey] ? <p>{tileContent[dateKey]}</p> : null;
   }, [tileContent]);
-  return <div><Calendar tileContent={tileValue} locale="ja-JP"/></div>;
+  return <CalendarWrapper><Calendar tileContent={tileValue} locale="ja-JP"/></CalendarWrapper>;
 };
 
