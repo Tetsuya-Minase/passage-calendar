14d658fcb5e253eb144f5e2a28bb3ed16eb5c14c
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-06-17 22:17:44 +0900

    feat: fix firebase auth.🔧

--- src/page/top/Top.tsx
+++ src/page/top/Top.tsx
@@ -3,7 +3,6 @@ import { InputForm } from './components/InputForm';
 import { initialState, formReducer, FormState } from '../../reducers/FormReducer';
 import { Dl } from '../../common/molecules/DescriptionList';
 import { FirebaseAuth, signInWithRedirect, signOut } from '../../common/util/FirebaseAuth';
-import { useAllDocuments } from '../../common/util/FirebaseDataBase';
 import { CalendarComponent } from '../../common/atoms/Calendar';
 
 export const Top = () => {
@@ -13,7 +12,6 @@ export const Top = () => {
   const Loading = useCallback(() => {
     return <div>loading now....</div>;
   }, []);
-  const { document, loaded } = useAllDocuments();
   const FormContext = createContext<FormState | undefined>(undefined);
   const DispatchContext = createContext<Dispatch<any> | undefined>(undefined);
   const [state, dispatch] = useReducer(formReducer, initialState);
@@ -21,14 +19,14 @@ export const Top = () => {
     <>
       <FirebaseAuth NotSignedIn={NotSignedIn} Loading={Loading}>
         <button onClick={signOut}>sign out</button>
-      </FirebaseAuth>
         <FormContext.Provider value={state}>
           <DispatchContext.Provider value={dispatch}>
             <InputForm dispatchContext={DispatchContext}/>
           </DispatchContext.Provider>
           {state.list.length === 0 ? null : <Dl items={state.list}/>}
         </FormContext.Provider>
-        <CalendarComponent {...document} />
+        <CalendarComponent />
+      </FirebaseAuth>
     </>
   );
 };
