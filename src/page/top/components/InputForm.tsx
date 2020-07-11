15da9c4538dd96241b29086e5a64b397026122e4
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-07-11 17:50:24 +0900

    style: remove unused imports.🧹

--- src/page/top/components/InputForm.tsx
+++ src/page/top/components/InputForm.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { ButtonComponent as Button } from '../../../common/atoms/Button';
 import { InputLabel } from '../../../common/molecules/InputLabel';
 import {
