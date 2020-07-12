204cc63f4f2069e3b1d76c6f50899fb14c7a2535
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-07-12 10:13:52 +0900

    feat: add top padding.🎨

--- src/page/top/Top.tsx
+++ src/page/top/Top.tsx
@@ -5,25 +5,32 @@ import { FirebaseAuth, signInWithRedirect, signOut } from '../../common/util/Fir
 import { CalendarComponent } from '../../common/atoms/calendar/Calendar';
 import { FormStateContext, SetFormStateContext, initialState } from '../../common/context/FormStateContext';
 import { Button } from '../../common/atoms/Button';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 0.5rem 1rem;
+`;
 
 export const Top = () => {
   const NotSignedIn = useCallback(() => {
-    return <Button text="sign in" click={() => signInWithRedirect()} size='small'/>
+    return <Button text="sign in" click={() => signInWithRedirect()} size='small'/>;
   }, []);
   const Loading = useCallback(() => {
     return <div>loading now....</div>;
   }, []);
   const [formState, setFormState] = useState(initialState);
   return (
-    <FirebaseAuth NotSignedIn={NotSignedIn} Loading={Loading}>
-      <Button text="sign out" click={signOut} size='small'/>
-      <FormStateContext.Provider value={formState}>
-        <SetFormStateContext.Provider value={setFormState}>
-          <InputForm/>
-          <Dl/>
-          <CalendarComponent/>
-        </SetFormStateContext.Provider>
-      </FormStateContext.Provider>
-    </FirebaseAuth>
+    <Wrapper>
+      <FirebaseAuth NotSignedIn={NotSignedIn} Loading={Loading}>
+        <Button text="sign out" click={signOut} size='small'/>
+        <FormStateContext.Provider value={formState}>
+          <SetFormStateContext.Provider value={setFormState}>
+            <InputForm/>
+            <Dl/>
+            <CalendarComponent/>
+          </SetFormStateContext.Provider>
+        </FormStateContext.Provider>
+      </FirebaseAuth>
+    </Wrapper>
   );
 };
